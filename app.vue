df66c202c9cb1a8fa3f8b4c4d25917e27cdb1bd8
이준용 <iiiuooyoo@gmail.com>
2023-03-24 10:23:35 +0900

    Initial commit

---
+++ app.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="w-[100vw] h-[100vh] flex items-center justify-center">
+        <div class="relative" @click="modalShow = true">
+            <span
+                v-for="(_, index) in temp"
+                :key="index"
+                class="text-center absolute triangle text-4xl"
+                :style="`left: ${setLeft(index)}px; top:${setTop(index)}px; `"
+            >
+                {{ config.icon }}
+            </span>
+        </div>
+        <ShareModal v-if="modalShow" ref="$modal" />
+    </div>
+</template>
+
+<script setup>
+import { range } from "@toss/utils";
+import { onClickOutside } from "@vueuse/core";
+
+const $modal = ref();
+const modalShow = ref(false);
+
+onClickOutside($modal, (event) => {
+    modalShow.value = false;
+});
+
+// 구성요소
+const config = reactive({
+    icon: "❤", // 표시할 아이콘
+    diameter: 100, // 지름
+    count: 50, // 아이콘 개수
+});
+
+// 각도
+const angle = computed(() => (Math.PI * 2) / config.count);
+
+// 아이콘 개수만큼 반복할 배열
+const temp = computed(() => range(config.count));
+
+const setLeft = (idx) =>
+    Math.ceil(config.diameter * Math.cos(idx * angle.value));
+const setTop = (idx) =>
+    Math.ceil(config.diameter * Math.sin(idx * angle.value));
+</script>
+
+<style lang="scss">
+.illusion-container {
+    border: 1px solid #000;
+    aspect-ratio: 1/1;
+    perspective: 1000px;
+}
+
+.triangle {
+    /* text-shadow: 0px 0px 5px #000; */
+    transform: rotate(0deg);
+
+    animation-name: TriangleRotate;
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+}
+</style>
---
+++ components/share/Modal.vue
@@ -0,0 +1,14 @@
+<template>
+    <div
+        class="modal-container p-4 absolute z-[100] bg-white rounded shadow-md cursor-pointer"
+    >
+        <div>어서오sdsdd고</div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+.modal-container {
+}
+</style>
